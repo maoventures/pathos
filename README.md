02cce6c7ed734466756568ce483b46ee3adc74a4
Theodore Nikolai Idris <lionelxho@gmail.com>
2021-09-15 18:13:44 +0800

    Add files via upload

--- README.md
+++ README.md
@@ -1,8 +1,34 @@
-# Pathos
-Rediscover your "why" as a maker. Pathos is an ever expanding digital and analogous toolkit that guides neurodivergent founders and creators build their own productivity and mental health solutions.
+This is a [Next.js](https://nextjs.org/) project bootstrapped with [`create-next-app`](https://github.com/vercel/next.js/tree/canary/packages/create-next-app).
 
-Designed by creator-maker duo, <a href="https://twitter.com/ismimao>@ismimao</a> and <a href="https://twitter.com/theonikomao">@theonikolai</a>, both digital and physical forms of the journal are completely customizable with delicately designed to provide both the structure and flexibility sorely missed from traditional products and methods.
+## Getting Started
 
-## Why is it open source?
+First, run the development server:
 
-Well, we recognize that everyone has different ideals for the *"perfect world,"* and we want **you** to have the chance to build it alongside us to ensure a cohesive experience.
+```bash
+npm run dev
+# or
+yarn dev
+```
+
+Open [http://localhost:3000](http://localhost:3000) with your browser to see the result.
+
+You can start editing the page by modifying `pages/index.tsx`. The page auto-updates as you edit the file.
+
+[API routes](https://nextjs.org/docs/api-routes/introduction) can be accessed on [http://localhost:3000/api/hello](http://localhost:3000/api/hello). This endpoint can be edited in `pages/api/hello.ts`.
+
+The `pages/api` directory is mapped to `/api/*`. Files in this directory are treated as [API routes](https://nextjs.org/docs/api-routes/introduction) instead of React pages.
+
+## Learn More
+
+To learn more about Next.js, take a look at the following resources:
+
+- [Next.js Documentation](https://nextjs.org/docs) - learn about Next.js features and API.
+- [Learn Next.js](https://nextjs.org/learn) - an interactive Next.js tutorial.
+
+You can check out [the Next.js GitHub repository](https://github.com/vercel/next.js/) - your feedback and contributions are welcome!
+
+## Deploy on Vercel
+
+The easiest way to deploy your Next.js app is to use the [Vercel Platform](https://vercel.com/new?utm_medium=default-template&filter=next.js&utm_source=create-next-app&utm_campaign=create-next-app-readme) from the creators of Next.js.
+
+Check out our [Next.js deployment documentation](https://nextjs.org/docs/deployment) for more details.
---
+++ tailwind.config.js
@@ -0,0 +1,89 @@
+const colors = require('tailwindcss/colors')
+const plugin = require('tailwindcss/plugin')
+
+module.exports = {
+  mode: 'jit',
+  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],  darkMode: false, // or 'media' or 'class'
+  theme: {
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      DEFAULT: '#1C2526',
+      // The colors, extended
+      blueGray: colors.blueGray,
+      coolGray: colors.coolGray,
+      trueGray: colors.trueGray,
+      warmGray: colors.warmGray,
+      orange: colors.orange,
+      amber: colors.amber,
+      lime: colors.lime,
+      emerald: colors.emerald,
+      teal: colors.teal,
+      cyan: colors.cyan,
+      sky: colors.sky,
+      violet: colors.violet,
+      fuschia: colors.fuschia,
+      rose: colors.rose,
+      // Default Colors
+      black: '#1C2526',
+      white: colors.white,
+      gray: colors.coolGray,
+      red: colors.red,
+      yellow: colors.amber,
+      green: colors.emerald,
+      blue: colors.blue,
+      indigo: colors.indigo,
+      purple: colors.violet,
+      pink: colors.pink,
+    },
+    extend: {
+      colors: {
+        'offWhite': '#FFFCEE',
+        'peach': '#FFEADC',
+        'pathGray': '#565E63',
+        'pathRed': '#A42100',
+        'pathRedark': '#932003',
+        'pathRedarkest': '#410E01',
+        'pathBlue': '#578287',
+        'pathBlueker': '#005058',
+        'pathBluekest': '#00383E',
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/typography'),
+    plugin(function({ addComponents }) {
+      const components = {
+        '.btn': {
+          padding: '.5rem 1rem',
+          borderRadius: '.3rem',
+          fontWeight: '600',
+          boxShadow: '0 2px 4px rgba(0,0,0,0.2)',
+          '&:hover': {
+            boxShadow: '0 4px 6px rgba(0,0,0,0.2)',
+          },
+        },
+        '.btn-primary': {
+          backgroundColor: '#578287',
+          color: '#fff',
+          '&:hover': {
+            backgroundColor: '#005058'
+          },
+        },
+        '.btn-secondary': {
+          backgroundColor: '#565E63',
+          color: '#fff',
+          '&:hover': {
+            backgroundColor: '#1C2526'
+          },
+        },
+      }
+
+      addComponents(components)
+    })
+  ]
+}
