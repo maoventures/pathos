2d325e207279cda120fb2e53630bc0f251eb2753
Theodore Nikolai Idris <theonikolaii@gmail.com>
2021-09-16 17:00:18 +0800

    Update 404.tsx

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,5 +1,5 @@
 export default function Custom404() {
   return <div className="justify-center items-center flex flex-col h-screen bg-warmGray-800 text-offWhite">
-    <h1 className="text-lg md:text-3xl font-thin">Hello, are you lost too?<br />Don't worry, we all get lost sometimes</h1>
+    <h1 className="text-lg md:text-3xl font-thin">Hello, are you lost too?<br />Don&apos;t worry, we all get lost sometimes</h1>
           </div>
 }
