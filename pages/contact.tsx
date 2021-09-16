8585480f7386f187f680fe07dd9752161405a9f8
Theodore Nikolai Idris <theonikolaii@gmail.com>
2021-09-16 17:08:06 +0800

    Update contact.tsx

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -4,32 +4,32 @@ export default function Contact() {
     <div className="container flex items-center justify-center flex-1 h-full mx-auto">
         <div className="w-full max-w-lg">
             <div className="leading-loose">
-                <form className="max-w-sm p-10 m-auto bg-white bg-opacity-25 rounded-xl shadow-xl" action="https://api.web3forms.com/submit" method="POST">
+                <form id="productform" className="max-w-sm p-10 m-auto bg-white bg-opacity-25 rounded-xl shadow-xl" action="https://api.web3forms.com/submit" method="POST">
                   <input type="hidden" name="apikey" value="9c1fa46e-ce14-468b-b9c2-7c8d5e600b30"/>
                   <input type="hidden" name="ccemail" value="hannah@pathos.asia"/>
                   <input type="hidden" name="subject" value="New Submission from Web3Forms" />
                   <input type="hidden" name="redirect" value="https://web3forms.com/success" />
                   <input type="hidden" name="recaptcha_response" id="recaptchaResponse"/>
-                  <input type="checkbox" name="botcheck" id="" class="hidden" />
+                  <input type="checkbox" name="botcheck" id="" className="hidden" />
                     <p className="mb-8 text-2xl font-light text-center text-white">
                         Contact Us
                     </p>
                     <div className="mb-2">
                         <div className="relative ">
-                          <label for="name">Your Name
-                            <input type="text" name="name" className="flex-1 w-full px-4 py-2 text-base text-gray-700 placeholder-gray-400 bg-white border border-transparent border-gray-300 rounded-lg shadow-sm appearance-none focus:outline-none focus:border-transparent" placeholder="John Doe"/></label>
+                          <label htmlfor="name">Your Name
+                            <input type="text" id="name" name="name" className="flex-1 w-full px-4 py-2 text-base text-gray-700 placeholder-gray-400 bg-white border border-transparent border-gray-300 rounded-lg shadow-sm appearance-none focus:outline-none focus:border-transparent" placeholder="John Doe"/></label>
                             </div>
                         </div>
                     <div className="mb-2">
                         <div className="relative ">
-                          <label for="email">Your Email
-                            <input type="email" name="email" className="flex-1 w-full px-4 py-2 text-base text-gray-700 placeholder-gray-400 bg-white border border-transparent border-gray-300 rounded-lg shadow-sm appearance-none focus:outline-none focus:border-transparent" placeholder="john@acme.inc"/></label>
+                          <label htmlfor="email">Your Email
+                            <input type="email" id="email" name="email" className="flex-1 w-full px-4 py-2 text-base text-gray-700 placeholder-gray-400 bg-white border border-transparent border-gray-300 rounded-lg shadow-sm appearance-none focus:outline-none focus:border-transparent" placeholder="john@acme.inc"/></label>
                             </div>
                         </div>
                         <div className="mb-2">
                             <div className="relative ">
-                              <label for="message">Your Message
-                                <input type="textarea" name="message" className="flex-1 w-full px-4 py-2 text-base text-gray-700 placeholder-gray-400 bg-white border border-transparent border-gray-300 rounded-lg shadow-sm appearance-none focus:outline-none focus:border-transparent" placeholder="Hi, I'd like to collaborate"/></label>
+                              <label htmlfor="message">Your Message
+                                <textarea form="productform" id="message" name="message" className="flex-1 w-full px-4 py-2 text-base text-gray-700 placeholder-gray-400 bg-white border border-transparent border-gray-300 rounded-lg shadow-sm appearance-none focus:outline-none focus:border-transparent" placeholder="Hi, I'd like to collaborate"/></label>
                                 </div>
                             </div>
                             <div className="flex items-center justify-between mt-4">
